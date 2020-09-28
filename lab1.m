6ce5ee777ceef8c0f0006270d19cd5aa50da5169
k-t-l-h <45423059+k-t-l-h@users.noreply.github.com>
2020-09-28 23:24:55 +0300

    preprocessing added

--- lab1.m
+++ lab1.m
@@ -1,2 +1,23 @@
- 
- 
+function lab1
+    matrix = magic(5);
+    preprocessing(matrix);
+end
+
+function m = preprocessing(m0)
+    m = m0;
+    
+    m = m';
+
+    % вычесть минимум из каждого столбца
+    for i = 1:5
+        display(min(m(i, :)));
+        m(i, :) = m(i, :) - min(m(i, :));
+    end
+    m = m';
+    
+    %вычесть минимум из каждой строки
+    for i = 1:5
+        m(i, :) = m(i, :) - min(m(i, :));
+    end
+    
+end
